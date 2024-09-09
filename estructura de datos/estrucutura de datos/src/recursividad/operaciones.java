81f2ba93afe81c96ee819f707470aa72d6e774c0
Angel Blas <alexisblas777@gmail.com>
2024-09-09 14:54:37 -0600

    logro: Creacion de menu y programa de Hola mundo recursivo

--- estructura de datos/estrucutura de datos/src/recursividad/operaciones.java
+++ estructura de datos/estrucutura de datos/src/recursividad/operaciones.java
@@ -2,4 +2,36 @@ package recursividad;
 
 public class operaciones {
 
+    private int n;
+
+    public operaciones (){
+    this.n=1;
+}
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = (n>0)?n:1;
+    }   
+    //metodo iterativo
+    public void imprimir (){
+        int i =1;
+        while (i<=n){
+            System.out.println("HOLA MUNDOTE");
+            i++;
+
+        }
+    }
+    public void imprimir (int n){
+        //caso base 
+        if (n==1) {
+            System.out.println("HOLA MUNDO");
+        }else{
+            //caso general
+            System.out.println("HOLA MUNDOTE");
+            imprimir(n-1);
+        }
+    }
 }
--- estructura de datos/estrucutura de datos/src/recursividad/testRecursividad.java
+++ estructura de datos/estrucutura de datos/src/recursividad/testRecursividad.java
@@ -1,7 +1,43 @@
 package recursividad;
 
+import javax.swing.JOptionPane;
+
 public class testRecursividad {
 public static void main(String[] args) {
-    
+    ejecutar();
+}
+public static String menu(){
+    String menu ="menu principal:\n"+
+            "1)METODO ITERATIVO\n"+
+            "2)METODO RECURSIVO\n"+
+            "3)SALIR\n"+
+            "ELIGE LA OPCION";
+            return JOptionPane.showInputDialog( menu);
+}
+public static void ejecutar (){
+    boolean sentinel = true;
+    String opcion ="";
+    operaciones op = new operaciones();
+    int n = 0;
+    do {
+    opcion =menu();
+    switch (opcion) {
+        case "1":
+        n = JOptionPane.showInputDialog("introduce el numero de veces a imprimir");
+        op.setN(n);
+        op.imprimir();
+        break;
+        case "2":
+        n = JOptionPane.showInputDialog("introduce el numero de veces a imprimir");
+        op.setN(n);
+        op.imprimir(op.getN());
+        break;
+        case "3"://salir pa
+        sentinel = false;
+        break;
+        default:
+        JOptionPane.showMessageDialog(null, "valor no valido");
+    }
+}while (sentinel);
 }
 }
